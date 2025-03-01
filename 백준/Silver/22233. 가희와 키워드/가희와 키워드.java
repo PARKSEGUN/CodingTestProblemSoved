e6d2ad17da41c7392b105813ba363f8dabc298ea
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2025-03-01 19:19:19 +0900

    [Silver III] Title: 가희와 키워드, Time: 2220 ms, Memory: 233864 KB -BaekjoonHub

---
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static StringTokenizer st;
+    private static StringBuilder sb = new StringBuilder();
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static int N, M;
+
+    private static Set<String> memo = new HashSet<>();
+
+    public static void main(String[] args) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            memo.add(br.readLine());
+        }
+        for (int i = 0; i < M; i++) {
+            String[] blogs = br.readLine().split(",");
+            for (int j = 0; j < blogs.length; j++) {
+                memo.remove(blogs[j]);
+            }
+            System.out.println(memo.size());
+        }
+    }
+}
