eb8bb8b674c98093fa2a2a3fff0e77c663118f29
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2025-03-19 12:53:54 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 26.60 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -5,6 +5,8 @@ class Solution {
     Map<String,int[]> giftInfo=new HashMap<>();
     Map<String,Integer> fromToCnt=new HashMap<>();
     Map<String,Integer> reciveCnt=new HashMap<>();
+    StringBuilder sb=new StringBuilder();
+
     public int solution(String[] friends, String[] gifts) {
         for(int i=0;i<friends.length;i++){
             giftInfo.put(friends[i],new int[] {0,0});
@@ -19,30 +21,23 @@ class Solution {
             giftInfo.get(to)[1]++;
             fromToCnt.put(gifts[i],fromToCnt.getOrDefault(gifts[i],0)+1);
         }
-        StringBuilder sb=new StringBuilder();
         for(int i=0;i<friends.length;i++){
             for(int j=i+1;j<friends.length;j++){
-                String first=friends[i];
-                String second=friends[j];
-                sb.append(first).append(" ").append(second);
-                String connFirstSecond=sb.toString();
-                int firstSecondCnt=fromToCnt.getOrDefault(connFirstSecond,0);
-                sb.setLength(0);
-                sb.append(second).append(" ").append(first);
-                String connSecondFirst=sb.toString();
-                int secondFirstCnt=fromToCnt.getOrDefault(connSecondFirst,0);
-                sb.setLength(0);
+                //각각 저장시켰던 정보 조회
+                int firstSecondCnt=getFromToCnt(friends[i],friends[j]);
+                int secondFirstCnt=getFromToCnt(friends[j],friends[i]);
+                //서로의 선물 주고받은 결과 확인
                 if(firstSecondCnt>secondFirstCnt){
-                    reciveCnt.put(first,reciveCnt.get(first)+1);
+                    reciveCnt.put(friends[i],reciveCnt.get(friends[i])+1);
                 }else if(firstSecondCnt<secondFirstCnt){
-                    reciveCnt.put(second,reciveCnt.get(second)+1);
+                    reciveCnt.put(friends[j],reciveCnt.get(friends[j])+1);
                 }else{ //같은 경우
-                    int firstScore=giftInfo.get(first)[0]-giftInfo.get(first)[1];
-                    int secondScore=giftInfo.get(second)[0]-giftInfo.get(second)[1];
+                    int firstScore=giftInfo.get(friends[i])[0]-giftInfo.get(friends[i])[1];
+                    int secondScore=giftInfo.get(friends[j])[0]-giftInfo.get(friends[j])[1];
                     if(firstScore>secondScore){
-                        reciveCnt.put(first,reciveCnt.get(first)+1);
+                        reciveCnt.put(friends[i],reciveCnt.get(friends[i])+1);
                     }else if(firstScore<secondScore){
-                        reciveCnt.put(second,reciveCnt.get(second)+1);
+                        reciveCnt.put(friends[j],reciveCnt.get(friends[j])+1);
                     }
                 }
                     
@@ -53,5 +48,13 @@ class Solution {
             answer=Math.max(answer,x);
         }
         return answer;
+    }
+    
+    private int getFromToCnt(String first, String second){
+        sb.append(first).append(" ").append(second);
+        String connFirstSecond=sb.toString();
+        int result=fromToCnt.getOrDefault(connFirstSecond,0);
+        sb.setLength(0);
+        return result;
     }
 }
