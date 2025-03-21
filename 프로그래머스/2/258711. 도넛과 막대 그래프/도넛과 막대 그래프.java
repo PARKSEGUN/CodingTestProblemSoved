19ace90eb21a20069b0904436808a52640d98be1
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2025-03-21 17:54:23 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 44.76 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -1,60 +1,27 @@
-import java.util.*;
-import java.io.*;
-
 class Solution {
-    Set<Integer> nodes=new HashSet<>();
-    Map<Integer,Integer> comeInCnt=new HashMap<>();
-    Map<Integer,Boolean> visited=new HashMap<>();
-    Map<Integer,Queue<Integer>> connInfo= new HashMap<>();
-    int donutCnt, poleCnt, eightCnt;
-    
+    static int N = 1_000_000;
+
     public int[] solution(int[][] edges) {
-        setting(edges);
-        int newNode=findNewNode();
-        Queue<Integer> nextNodes=connInfo.get(newNode);
-        while(!nextNodes.isEmpty()){
-            int next=nextNodes.poll();
-            visited.put(next,true);
-            int result=findConnNode(next);
-            if(result==0) poleCnt++;
-            else if(result==1)donutCnt++;
-            else if(result==2) eightCnt++;
+        int[] ingoing = new int[N];
+        int[] outgoing = new int[N];
+        for(int[] edge : edges) {
+            outgoing[edge[0]-1]++;
+            ingoing[edge[1]-1]++;
         }
-        return new int[] {newNode,donutCnt,poleCnt,eightCnt};
-    }
-    
-    private int findNewNode(){
-        for(int cur:nodes){
-            if(comeInCnt.get(cur)==0&&connInfo.get(cur).size()>1) return cur;
+        int created = 0;
+        int eight = 0;
+        int stick = 0;
+        for(int i=0; i<N; i++) {
+            if(outgoing[i] >= 2) {
+                if(ingoing[i] == 0) {
+                    created = i;
+                }else {
+                    eight++;
+                }
+            }else if(outgoing[i] == 0 && ingoing[i] > 0){
+                stick++;
+            }
         }
-        return -1;
-    }
-    
-    private int findConnNode(int cur){
-        int result=0;
-        Queue<Integer> nextNodes=connInfo.get(cur);
-        while(!nextNodes.isEmpty()){
-            int next=nextNodes.poll();
-            if(visited.get(next)) result++;
-            visited.put(next,true);
-            result+=findConnNode(next);
-        }
-        return result;
-    }
-    
-    private void setting(int[][] edges){
-        for(int i=0;i<edges.length;i++){
-            int from=edges[i][0];
-            int to=edges[i][1];
-            nodes.add(from);
-            nodes.add(to);
-            visited.putIfAbsent(from,false);
-            visited.putIfAbsent(to,false);
-            comeInCnt.putIfAbsent(from,0);
-            comeInCnt.put(to,comeInCnt.getOrDefault(to,0)+1);
-            connInfo.putIfAbsent(from,new ArrayDeque<>());
-            connInfo.get(from).add(to);
-            connInfo.putIfAbsent(to,new ArrayDeque<>());
-        }
+        return new int[] {created+1,outgoing[created]-eight-stick,stick,eight};
     }
 }
