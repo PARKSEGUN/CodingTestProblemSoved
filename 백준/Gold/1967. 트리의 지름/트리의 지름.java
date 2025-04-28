81ae0f186aab581ef30461e599b07cc071a4d3f9
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2025-04-28 14:23:23 +0900

    [Gold IV] Title: 트리의 지름, Time: 204 ms, Memory: 22784 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.java
@@ -1,7 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -18,34 +21,27 @@ public class Main {
     }
 
     private static void solution() {
+        boolean[] visited = new boolean[N + 1];
         //1에서 가장 멀리 떨어진 노드 찾기
-        int[] tmp = getFarNode(1);
-        int[] result = getFarNode(tmp[0]);
+        int[] tmp = new int[]{0, 0};
+        tmp = getFarNode(1, 0, tmp, visited);
+
+        Arrays.fill(visited, false);
+        int[] result = getFarNode(tmp[0], 0, tmp, visited);
         answerString.append(result[1]);
 
     }
 
-    private static int[] getFarNode(int startNode) {
-        Queue<int[]> queue = new ArrayDeque<>();
-        queue.add(new int[]{startNode, 0});
-        boolean[] visited = new boolean[N + 1];
-        visited[startNode] = true;
-        int[] result = {0, 0};
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int num = cur[0];
-            int val = cur[1];
-
-            if (result[1] < val) {
-                result[1] = val;
-                result[0] = num;
-            }
-            for (int i = 0; i < connInfo.get(num).size(); i++) {
-                int[] next = connInfo.get(num).get(i);
-                if (visited[next[0]]) continue;
-                visited[next[0]] = true;
-                queue.add(new int[]{next[0], val + next[1]});
-            }
+    private static int[] getFarNode(int num, int val, int[] result, boolean[] visited) {
+        visited[num] = true;
+        if (result[1] < val) {
+            result[0] = num;
+            result[1] = val;
+        }
+        for (int i = 0; i < connInfo.get(num).size(); i++) {
+            int[] next = connInfo.get(num).get(i);
+            if (visited[next[0]]) continue;
+            getFarNode(next[0], val + next[1], result, visited);
         }
         return result;
     }
