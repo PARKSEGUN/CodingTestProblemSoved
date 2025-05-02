e06d4f8e47b4a376017c33d4b7f06052f6a3e57a
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2025-05-02 19:28:44 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 4392 ms, Memory: 162908 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.java
@@ -19,8 +19,8 @@ public class Main {
     }
 
     private static void solution() throws IOException {
-        double[] ab = new double[N * N];
-        double[] cd = new double[N * N];
+        int[] ab = new int[N * N];
+        int[] cd = new int[N * N];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 ab[i * N + j] = map[i][0] + map[j][1];
@@ -28,23 +28,42 @@ public class Main {
             }
         }
 
-        long result = 0;
-//        Arrays.sort(ab);
+        Arrays.sort(ab);
         Arrays.sort(cd);
-//        System.out.println(Arrays.toString(ab));
-//        System.out.println(Arrays.toString(cd));
-        for (int i = 0; i < ab.length; i++) {
-            //-ab[i]에 해당하는 값이 여러개 있는 경우를 생각해줘야한다
-//            System.out.println(-ab[i]);
-//            System.out.println(Arrays.binarySearch(cd, -ab[i]));
-            if (Arrays.binarySearch(cd, (ab[i] == 0.0) ? 0.0 : -(ab[i])) >= 0) {
 
-                int startIdx = -(Arrays.binarySearch(cd, -ab[i] - 0.5)) - 1;
-                int endIdx = -(Arrays.binarySearch(cd, -ab[i] + 0.5)) - 1;
-                result += endIdx - startIdx;
+        int start = 0;
+        int end = cd.length - 1;
+        long result = 0;
+
+        while (start < ab.length && end >= 0) {
+            int sum = ab[start] + cd[end];
+
+            if (sum == 0) {
+                int abVal = ab[start];
+                int cdVal = cd[end];
+                long abCount = 0;
+                long cdCount = 0;
+
+                // ab에서 중복 개수 세기
+                while (start < ab.length && ab[start] == abVal) {
+                    abCount++;
+                    start++;
+                }
+
+                // cd에서 중복 개수 세기
+                while (end >= 0 && cd[end] == cdVal) {
+                    cdCount++;
+                    end--;
+                }
+
+                result += abCount * cdCount;
+
+            } else if (sum < 0) {
+                start++;
+            } else { // sum > 0
+                end--;
             }
         }
-
         answerString.append(result);
     }
 
