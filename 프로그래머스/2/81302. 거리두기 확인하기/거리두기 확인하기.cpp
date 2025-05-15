b5c77e7ab9c496d010c3a16606deed5a10c4f699
박세건 <90709751+PARKSEGUN@users.noreply.github.com>
2025-05-15 18:58:16 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.cpp
@@ -0,0 +1,66 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int dix[4] = { 0,0,1,-1 };
+int diy[4] = { -1,1,0,0 };
+
+bool BFS(int sx, int sy, vector<string>& place) {
+    queue<pair<int, int>> move;
+    queue<int> count;
+    move.push({ sx,sy });
+    count.push(0);
+    bool visited[5][5] = { 0, };
+    visited[sx][sy] = true;
+    while (move.size() != 0) {
+        int x = move.front().first;
+        int y = move.front().second;
+        int cnt = count.front();
+        move.pop();
+        count.pop();
+        for (int i = 0; i < 4; i++) {
+            int dx = x + dix[i];
+            int dy = y + diy[i];
+            if (dx >= 0 && dx < 5 && dy >= 0 && dy < 5) {
+                if (visited[dx][dy] == false && cnt + 1 <= 2 && place[dx][dy] != 'X') {
+                    if (place[dx][dy] == 'P')
+                        return false;
+                    visited[dx][dy] = true;
+                    move.push({ dx,dy });
+                    count.push(cnt + 1);
+                }
+            }
+        }
+    }
+    return true;
+}
+
+bool solve(vector<string>& place) {
+    vector<pair<int, int>> people;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (place[i][j] == 'P')
+                people.push_back({ i,j });
+        }
+    }
+    for (int i = 0; i < people.size(); i++) {
+        if (BFS(people[i].first, people[i].second, place) == false) {
+            return false;
+        }
+    }
+    return true;
+}
+
+vector<int> solution(vector<vector<string>> places) {
+    vector<int> answer;
+    for (int i = 0; i < places.size(); i++) {
+        if (solve(places[i]) == true)
+            answer.push_back(1);
+        else
+            answer.push_back(0);
+    }
+    return answer;
+}
